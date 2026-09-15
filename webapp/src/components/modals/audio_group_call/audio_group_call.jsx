2128f75e68b3927dcefa9946f95a4e86d1dae1ad
Lenerson Velho Nunes <lenerson.nunes@lepton.tech>
2026-09-15 13:28:50 -0300

    fix(ci): Place session adapter documentation

--- webapp/src/components/modals/audio_group_call/audio_group_call.jsx
+++ webapp/src/components/modals/audio_group_call/audio_group_call.jsx
@@ -144,11 +144,9 @@ export class AudioCallPanel extends React.Component {
                 this.setState({peerStreams});
             }
         });
-
-
+        // Transitional adapters keep the component tests focused on visible
+        // behaviour while resource ownership moves into VoiceSession.
         Object.defineProperties(this, {
-            // Transitional adapters keep the component tests focused on visible
-            // behaviour while resource ownership moves into VoiceSession.
             swarmInstance: {
                 get: () => this.voiceSession.swarm,
                 set: (value) => {
